87749d5bbc9bd92b838bb9b244c4e220af4701e1
palashshrote <palash.shrote.58@gmail.com>
2022-04-16 17:55:14 +0530

    2nd commit in pipeline_one

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
+from crypt import methods
 from distutils.log import error
+import imp
+from unicodedata import name
+from wsgiref.validate import validator
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+#create a Form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
 
 
 
@@ -25,3 +41,16 @@ def user(name):
 def page_not_found(e):
     return render_template('error.html') 
 
+
+#Create a name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Vaildate form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", 
+            name = name,
+            form = form)
